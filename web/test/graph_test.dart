d4c0e47a1e7b86c00df7a8c878ee3ed6f22881c3
onedayitwillmake <mariogonzalez@gmail.com>
2012-10-26 01:29:28 -0400

    Created initial unittest for graph

---
+++ web/test/graph_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'package:unittest/unittest.dart' as UnitTest;
+import '../oneday/ds/ds.dart';
+import '../oneday/geom/geom.dart';
+
+void main() {
+  
+  UnitTest.test("Load graph from file system", (){
+    
+  File file = new File("../../data/simpleGraph01.txt");
+  Future<String> finishedReading = file.readAsText( Encoding.ASCII );
+  
+  finishedReading.then(UnitTest.expectAsync1((text){
+    var expected = "1:54322:413:514:6215:109316:8747:868:769:1110510:119511:109"; // Trimmed result
+    var graph = new GGraph.fromSimpleText( text, false );
+    var result = graph.toString().replaceAll(new RegExp('\t|\n'), "");
+    UnitTest.expect(result, UnitTest.equals(expected));
+    }));
+  });
+}
